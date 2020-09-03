6ea1475aca2fb7bf8cf2f705d2d0d55229bd703d
Alex Koziol <akoziol21@webb.edu>
2020-09-02 22:18:01 -0400

    added architecture files

---
+++ tests/context.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../electrical_systems_model"))
+
+# import <module>
+
+# use the following syntax for tests:
+# from .context import <module>
