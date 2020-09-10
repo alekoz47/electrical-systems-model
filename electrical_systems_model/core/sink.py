dbe7afeaec699b8280e2e8b1bae8bca45794d148
bhunt21 <bhunt21@webb.edu>
2020-09-10 15:03:25 -0400

    Finished making the sinks class

--- electrical_systems_model/core/sink.py
+++ electrical_systems_model/core/sink.py
@@ -2,21 +2,21 @@ from core.component import Component
 from core.power import Power
 
 
-class Sinks:
-    def __init__(self, parents, children, power, location):
-        super(Sinks).__init__(parents, children)
+class Sinks(Component):
+    def __init__(self, location, parents, children, power):
+        super().__init__(location, parents, children)
         self.power_in = power
-        self.location = location  # location is a 3d vector
+
 
 
 class MechanicalSink(Sinks):
-    def __init__(self, parents, children, power_in, location):
-        super(MechanicalSink).__init__(parents, children, power_in, location)
+    def __init__(self, location, parents, children, power):
+        super().__init__(parents, children, power, location)
 
 
 class ElectricalSink(Sinks):
-    def __init__(self, parents, children, power_in, location, voltage, phase):
-        super(ElectricalSink).__init__(parents, children, power_in, location)
+    def __init__(self, location, parents, children, power_in, voltage, phase):
+        super().__init__(location, parents, children, power_in)
         self.voltage_level = voltage
         self.phase = phase
 
