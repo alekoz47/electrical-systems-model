de3e483b1e55307f612b70b5ba1fccd0221a4e92
Alex Koziol <akoziol21@webb.edu>
2020-09-10 15:10:12 -0400

    added initial source and generator definitions

--- electrical_systems_model/core/source.py
+++ electrical_systems_model/core/source.py
@@ -1,2 +1,18 @@
 from core.component import Component
 from core.power import Power
+
+
+class Source(Component):
+    def __init__(self, location, parents, children, power):
+        super().__init__(location, parents, children)
+        self.power_in = power
+
+    def get_power_in(self):
+        pass
+
+
+# TODO: implement electrical-mechanical power transfer
+#   this means we need both a Diesel and a Generator
+class Generator(Source):
+    def __init__(self, location, parents, children, power):
+        super().__init__(parents, children, power, location)
