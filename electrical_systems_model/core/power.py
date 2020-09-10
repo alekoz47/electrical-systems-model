548150c0bf2687b1916103fdf44fe4efda2b448f
bhunt21 <bhunt21@webb.edu>
2020-09-10 15:40:55 -0400

    Updated the power.py classes and implemented the Transformer class

--- electrical_systems_model/core/power.py
+++ electrical_systems_model/core/power.py
@@ -1,51 +1,42 @@
 
 
 class Power:
-    def __init__(self):
-        self.power = None
+    def __init__(self, power):
+        self.power = power
 
 
 class ElectricPower(Power):
-    def __init__(self):
-        super().__init__()
-        self.voltage = None
+    def __init__(self, power, voltage):
+        super().__init__(power)
+        self.voltage = voltage
 
 
 class AlternatingCurrent(ElectricPower):
-    def __init__(self):
-        super().__init__()
-        self.frequency = None
-        self.power_factor = None
+    def __init__(self, power, voltage, frequency, power_factor):
+        super().__init__(power, voltage)
+        self.frequency = frequency
+        self.power_factor = power_factor
 
 
 class SinglePhase(AlternatingCurrent):
-    def __init__(self):
-        super().__init__()
-
-    def current(self):
+    def __init__(self, power, voltage, frequency, power_factor):
+        super().__init__(power, voltage,frequency,power_factor)
         self.current = self.power_factor * self.power / self.voltage
-        return self.current
 
 
 class ThreePhase(AlternatingCurrent):
-    def __init__(self):
-        super().__init__()
-
-    def current(self):
+    def __init__(self, power, voltage, frequency, power_factor):
+        super().__init__(power, voltage, frequency, power_factor)
         self.current = numpy.sqrt(3) * self.power_factor * self.power / self.voltage
-        return self.current
 
 
 class DirectCurrent(ElectricPower):
     def __init__(self):
-        super().__init__()
-
-    def current(self):
+        super().__init__(power, voltage)
         self.current = self.power / self.voltage
-        return self.current
 
 
 class MechanicalPower(Power):
     def __init(self):
-        super().__init__()
-        self.rpm = None
+        super().__init__(power, rpm)
+        self.rpm = rpm
