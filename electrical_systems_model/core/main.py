b0804d87e4489f275b54b6ceceb6ad230ca601f6
Alex Koziol <akoziol21@webb.edu>
2020-09-11 10:47:22 -0400

    updated main tests for new component constructor

--- electrical_systems_model/core/main.py
+++ electrical_systems_model/core/main.py
@@ -3,30 +3,28 @@ from core.sink import ElectricalSink
 from core.power import AlternatingCurrent
 from core.transmission import Transformer
 
-test = Sink(1,2,3,4)
+test = Sink(1, 2)
 
-print(test._parents)
-print(test._children)
 print(test.location)
 print(test.power_in)
 
-test2 = ElectricalSink(1,2,3,4,5,6)
+test2 = ElectricalSink(1, 2, 3, 4)
 
-#parents, children, power, location, voltage, phase
+# power, location, voltage, phase
+test_array = [test2.location, test2.power_in, test2.voltage_level, test2.phase]
+assert test_array == [1, 2, 3, 4]
+print(', '.join(map(str, test_array)))
 
-print(test2.location, test2._parents, test2._children, test2.power_in, test2.voltage_level, test2.phase)
-
-power = AlternatingCurrent(1000,2,3,4)
+power = AlternatingCurrent(1000, 2, 3, 4)
 print(power.power, power.voltage, power.frequency, power.power_factor)
 
-transformer = Transformer(1,2,3)
+transformer = Transformer(1, 2)
 transformer.get_power_in()
 print(transformer.power_in.power)
 print(transformer.power_in.voltage)
 
 
-#if __name__ == "__main__":
-    #test_component = Component()
-    #print(test_component.get_children())
-    #print(test_component)
-
+# if __name__ == "__main__":
+#     test_component = Component()
+#     print(test_component.get_children())
+#     print(test_component)
