37065ca005865311a9f59aceb446367a1134b2cf
Alex Koziol <akoziol21@webb.edu>
2020-09-15 00:09:31 -0400

    implemented add_power methods

--- electrical_systems_model/core/power.py
+++ electrical_systems_model/core/power.py
@@ -1,9 +1,14 @@
 import numpy
+from abc import abstractmethod
 
 
 class Power:
     def __init__(self, power):
         self.power = power
+
+    @abstractmethod
+    def add(self, power):
+        pass
 
 
 class ElectricPower(Power):
@@ -11,12 +16,20 @@ class ElectricPower(Power):
         super().__init__(power)
         self.voltage = voltage
 
+    def add(self, power):
+        self.power = self.power + power
+
 
 class AlternatingCurrent(ElectricPower):
-    def __init__(self, power, voltage, frequency, power_factor):
+    def __init__(self, power, voltage, frequency, power_factor=1):
         super().__init__(power, voltage)
+        self.power = complex(power, 0)
         self.frequency = frequency
         self.power_factor = power_factor
+
+    def add(self, power):
+        super().add(power)
+        self.power_factor = self.power.real / abs(self.power)
 
 
 class SinglePhase(AlternatingCurrent):
@@ -24,10 +37,18 @@ class SinglePhase(AlternatingCurrent):
         super().__init__(power, voltage, frequency, power_factor)
         self.current = self.power_factor * self.power / self.voltage
 
+    def add(self, power):
+        super().add(power)
+        self.current = self.power_factor * self.power / self.voltage
+
 
 class ThreePhase(AlternatingCurrent):
     def __init__(self, power, voltage, frequency, power_factor):
         super().__init__(power, voltage, frequency, power_factor)
+        self.current = numpy.sqrt(3) * self.power_factor * self.power / self.voltage
+
+    def add(self, power):
+        super().add(power)
         self.current = numpy.sqrt(3) * self.power_factor * self.power / self.voltage
 
 
@@ -41,3 +62,6 @@ class MechanicalPower(Power):
     def __init(self, power, rpm):
         super().__init__(power)
         self.rpm = rpm
+
+    def add(self, power):
+        pass
