f91271a68548e96cadefaaef50e4c389ed81de95
Alex Koziol <=>
2020-09-15 22:27:59 -0400

    updated constructor for ElectricalSink

--- electrical_systems_model/core/sink.py
+++ electrical_systems_model/core/sink.py
@@ -1,14 +1,14 @@
 from core.component import Component
-from core.power import Power
+from core.power import ThreePhase, SinglePhase
 
 
 class Sink(Component):
     def __init__(self, location, power):
         super().__init__(location)
-        self.power_in = power
+        self.power_in = None
 
     def get_power_in(self):
-        return self.power_in
+        pass
 
 
 class MechanicalSink(Sink):
@@ -17,7 +17,19 @@ class MechanicalSink(Sink):
 
 
 class ElectricalSink(Sink):
-    def __init__(self, location, power_in, voltage, phase=3):
+    def __init__(self, location, power_in, voltage, power_factor=1, frequency=60, phase=3):
         super().__init__(location, power_in)
-        self.voltage_level = voltage
+        self.power_factor = power_factor
+        self.frequency = frequency
+        self.voltage = voltage
         self.phase = phase
+
+    def get_power_in(self):
+        if self.phase == 0:
+            pass
+        elif self.phase == 1:
+            self.power_in = SinglePhase(self.power_in, self.voltage, self.frequency, self.power_factor)
+        elif self.phase == 3:
+            self.power_in = ThreePhase(self.power_in, self.voltage, self.frequency, self.power_factor)
+        else:
+            print("Please enter a valid phase (DC: 0, Single: 1, Triple: 3)")
